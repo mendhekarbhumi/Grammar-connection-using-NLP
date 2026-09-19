d3e06b0d44da644455ceef0511c1310f7418a59d
mendhekarbhumi <mendhekarbhumi@gmail.com>
2026-09-19 19:59:23 +0530

    Create grammar_nlp.py

---
+++ grammar_nlp.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+sentence = input("Enter a sentence: ")
+
+words = word_tokenize(sentence)
+tags = pos_tag(words)
+
+print("\nWords and their Grammar Tags:")
+for word, tag in tags:
+    print(word, ":", tag)
